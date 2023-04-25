3f69d12cfeaaa8dc26ef21f0f4d65583206fed5f
James Hawk <jph94880@gmail.com>
2023-04-25 01:06:57 -0500

    Adds my file.md

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+/*function addingEventListener() {
+}*/
+
+
+function addingEventListener(){
+    const clickAlert = document.getElementById('button');
+    clickAlert.addEventListener('click', function() {
+        alert('I was clicked');
+    });
 }
